1d79af91be30e33a183baa2e7b5e4a2ba0bbd266
lcmuri <lucinda.wanjiru@student.moringaschool.com>
2025-05-08 11:34:06 +0300

    Removed modules folder from git main branch

--- Modules/Medicine/resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-<x-medicine::layouts.master>
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('medicine.name') !!}</p>
-</x-medicine::layouts.master>
--- Modules/Medicine/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Medicine\Http\Controllers\MedicineController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('medicine', MedicineController::class)->names('medicine');
-});
